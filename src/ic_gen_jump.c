167a6f655c85654f31c70ae465766445c683fba9
Moritz Röhrich <moroeh@t-online.de>
2020-03-13 14:40:36 +0100

    IC GEN: Fixed return statements without values.
    Same as the fix in assembly generation.

--- src/ic_gen_jump.c
+++ src/ic_gen_jump.c
@@ -12,7 +12,9 @@
 #include "ic_gen.h"
 
 quad_list* ic_gen_ret(asn* node){
-    quad_list* IC = ic_gen(node->op.ret_exp.val);
+    quad_list* IC = NULL;
+	if(node->op.ret_exp.val != NULL)
+		IC = ic_gen(node->op.ret_exp.val);
     quadruple* q = make_quad(fac_ret, NULL, NULL, NULL);
     quad_list_app_quad(&IC, q);
     return IC;
