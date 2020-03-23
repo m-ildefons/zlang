6477fea6cc5cf9af802de104bc1f75ef9ddbe6ea
Moritz Röhrich <moroeh@t-online.de>
2020-03-23 22:41:53 +0100

    Symbol List: New, simpler data structure for symbol handling
    Added a new, simpler data structure for symbol handling. Most
    importantly, it fixes a nasty bug, where symbol maps that are already
    finished getting generated do not get updated, when the upper level
    symbol map is expanded.

---
+++ src/symbol_list.c
@@ -0,0 +1,156 @@
+/*
+ * FILENAME: symbol_list.c
+ *
+ * DESCRIPTION:
+ *     Implementations of functions handling the symbol lists.
+ *
+ * AUTHOR: Moritz Röhrich <moritzr@pool.math.tu-berlin.de>
+ *
+ */
+
+#include "symbol_list.h"
+
+symbol* new_symbol(const char* id, atomic_type type){
+    symbol* s = malloc(sizeof(symbol));
+    s->ref_count = 1;
+    s->ident = strdup(id);
+    s->scope = 0;
+    s->type = type;
+    return s;
+}
+
+symbol_list_entry* new_symbol_list_entry(){
+    symbol_list_entry* e = malloc(sizeof(symbol_list_entry));
+    assert(e != NULL);
+    e->next = NULL;
+    e->ref_count = 1;
+    e->sym = NULL;
+    return e;
+}
+
+symbol_list* new_symbol_list(size_t scope){
+    symbol_list* sl = malloc(sizeof(symbol_list));
+    assert(sl != NULL);
+    sl->top = NULL;
+    sl->bottom = NULL;
+    sl->scope = scope;
+
+    symbol* ph = new_symbol(".placeholder", at_void);
+    symbol_list_append(&sl, &ph);
+    delete_symbol(&ph);
+
+    return sl;
+}
+
+void delete_symbol(symbol** s){
+    if((*s) == NULL)
+        return;
+
+    (*s)->ref_count--;
+    if((*s)->ref_count > 0)
+        return;
+
+    free((*s)->ident);
+    free((*s));
+    (*s) = NULL;
+}
+
+void delete_symbol_list_entry(symbol_list_entry** e){
+    if((*e) == NULL)
+        return;
+
+    (*e)->ref_count--;
+    if((*e)->ref_count > 0)
+        return;
+
+    if((*e)->sym != NULL)
+        delete_symbol(&((*e)->sym));
+
+    free((*e));
+    (*e) = NULL;
+}
+
+void delete_symbol_list(symbol_list* sl){
+    if(sl == NULL)
+        return;
+
+    if(sl->top == NULL && sl->bottom == NULL){
+        free(sl);
+        return;
+    }
+
+    if(sl->bottom->next != NULL)
+        sl->bottom->next->ref_count--;
+
+    sl->bottom->next = NULL;
+    delete_symbol_list_entry(&(sl->bottom));
+
+    symbol_list_entry* e = NULL;
+    symbol_list_entry* next = NULL;
+
+    e = sl->top;
+    while(e != NULL){
+        next = e->next;
+        delete_symbol_list_entry(&e);
+        e = next;
+    }
+
+    free(sl);
+}
+
+void print_symbol_list_entry(const symbol_list_entry* e){
+    printf("[%zu] %s %s\n",
+        e->sym->scope,
+        atomic_type_cn[e->sym->type],
+        e->sym->ident);
+}
+
+void print_symbol_list(const symbol_list* sl){
+    printf("--\n");
+    symbol_list_entry* e = sl->top;
+    for(; e != NULL; e = e->next){
+        if(e == sl->top)
+            printf("t ->");
+        else if(e == sl->bottom)
+            printf("b ->");
+        else
+            printf("    ");
+        print_symbol_list_entry(e);
+    }
+    printf("--\n");
+}
+
+void symbol_list_insert(symbol_list** sl, symbol_list_entry** e){
+    if((*sl)->top == NULL){
+        (*sl)->top = (*e);
+        (*e)->ref_count++;
+    }
+
+    if((*sl)->bottom != NULL){
+        (*e)->next = (*sl)->bottom->next;
+        (*sl)->bottom->next = (*e);
+        (*sl)->bottom->next->ref_count++;
+        (*sl)->bottom->ref_count--;
+    }
+
+    (*sl)->bottom = (*e);
+    (*e)->ref_count++;
+}
+
+void symbol_list_append(symbol_list** sl, symbol** s){
+    symbol_list_entry* e = new_symbol_list_entry();
+    e->sym = (*s);
+    e->sym->ref_count++;
+    symbol_list_insert(sl, &e);
+    delete_symbol_list_entry(&e);
+}
+
+symbol* search_symbol(const symbol_list* sl, const char* key){
+    symbol_list_entry* ptr = sl->top;
+    for(; ptr != NULL; ptr = ptr->next){
+        if(strcmp(ptr->sym->ident, key) == 0)
+            return ptr->sym;
+    }
+    return NULL;
+}
+
---
+++ src/symbol_list.h
@@ -0,0 +1,90 @@
+/*
+ * FILENAME: symbol_list.h
+ *
+ * DESCRIPTION:
+ *     Delarations for datastructures and functions for symbol lists.
+ *
+ *     This implementation is based on 'split headed linked lists'. That means,
+ *     it is basically a linked list, but also remembers its own bottom entry,
+ *     which then may link to the top entry of another such list. This allowes
+ *     mutiple lists to link to the same bottom list, thus making the whole list
+ *     construction have a split head. With the top list containing inner scope
+ *     symbols and the bottom list conatining more global scoped symbols, this
+ *     mirrors the desired scoping behavior. Modifying a bottom list also
+ *     'updates' information in the top lists. Shadowing works too, because
+ *     being single linked lists, the innermost scope is always searched first.
+ *
+ *     Saving a pointer to the last entry of the list itself (the last of the
+ *     inner most scope), one can easily determine which scope one is currently
+ *     looking at.
+ *
+ *     The only drawback a priori is that the time for searching is linear with
+ *     the sum of entries in all lists. If this turns out to be a performance
+ *     bottleneck, shortlists saving the last accessed entries can be used to
+ *     accellerate this structure a bit.
+ *
+ * AUTHOR: Moritz Röhrich <moritzr@pool.math.tu-berlin.de>
+ *
+ */
+
+#ifndef __SYMBOL_LIST_H__
+#define __SYMBOL_LIST_H__
+
+#include <stdio.h>
+#include <assert.h>
+
+#include "strop.h"
+#include "atomic_type.h"
+#include "int_stack.h"
+
+
+/* Symbol
+ *
+ * All you need to know about something described by an identifier
+ */
+typedef struct {
+    size_t ref_count;
+    char* ident;
+    size_t scope;
+    atomic_type type;
+} symbol;
+
+/* Symbol List Entry
+ *
+ * This is just a management structure ancapsulating the actual symbol list
+ */
+typedef struct s_symbol_list_entry_t {
+    struct s_symbol_list_entry_t* next;
+    size_t ref_count;
+    symbol* sym;
+} symbol_list_entry;
+
+/* Symbol List
+ *
+ * The symbol list remembers top, bottom and scope of symbol list of the
+ * current scope.
+ */
+typedef struct s_symbol_list_t {
+    struct s_symbol_list_entry_t* top;
+    struct s_symbol_list_entry_t* bottom;
+    size_t scope;
+} symbol_list;
+
+/* imeplemented in symbol_list.c */
+symbol* new_symbol(const char* id, atomic_type type);
+symbol_list_entry* new_symbol_list_entry(void);
+symbol_list* new_symbol_list(size_t scope);
+
+void delete_symbol(symbol** s);
+void delete_symbol_list_entry(symbol_list_entry** e);
+void delete_symbol_list(symbol_list* sl);
+
+void print_symbol_list_entry(const symbol_list_entry* e);
+void print_symbol_list(const symbol_list* sl);
+
+void symbol_list_insert(symbol_list** sl, symbol_list_entry** e);
+void symbol_list_append(symbol_list** sl, symbol** s);
+symbol* search_symbol(const symbol_list* sl, const char* key);
+
+#endif
+
